649767e2019491e01ea947422b73828d6b2f6bb6
scott <scottadamsmith1@gmail.com>
2025-12-01 12:30:12 -0500

    Add git configs to install.sh

--- install.sh
+++ install.sh
@@ -6,11 +6,10 @@ dir="$(dirname "$(readlink -f "$0")")/"
 
 cd $HOME
 ln -s $dir.bash_logout $dir.bash_profile \
-	$dir.bashrc $dir.inputrc $dir.prettierrc \
-	$dir.profile $dir.vimrc .
+	$dir.bashrc $dir.gitconfig $dir.gittemplate/ \
+	$dir.inputrc $dir.prettierrc $dir.profile $dir.vimrc .
 
 cd $XDG_CONFIG_HOME
-ln -s $dir.config/hypr/ $dir.config/kitty/ \
-	$dir.config/waybar/ .
+ln -s $dir.config/hypr/ $dir.config/kitty/ $dir.config/waybar/ .
 
 echo 'Reboot system to reload hyprland and view changes.'

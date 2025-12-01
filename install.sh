9908938201836d2244e1d7498799bea4759c974c
scott <scottadamsmith1@gmail.com>
2025-12-01 11:37:12 -0500

    Fix up symlinks. Must be created in actual directory to work

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-cd "$(dirname "$(readlink -f "$0")")/"
+set -e
 
-# Add symlinks to user home directory
-ln -s .bash_logout .bash_profile .bashrc .inputrc .pretterrc .profile .vimrc $HOME
-# ln -s .config/hypr/ .config/kitty/ .config/waybar/ $XDG_CONFIG_HOME
+dir="$(dirname "$(readlink -f "$0")")/"
+
+cd $HOME
+ln -s $dir.bash_logout $dir.bash_profile $dir.bashrc $dir.inputrc $dir.prettierrc $dir.profile $dir.vimrc .
+
+cd $XDG_CONFIG_HOME
+ln -s $dir.config/hypr/ $dir.config/kitty/ $dir.config/waybar/ .

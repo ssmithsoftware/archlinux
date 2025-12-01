a0067ea556d773e6d530ec8c600da32d8138162f
scott <scottadamsmith1@gmail.com>
2025-12-01 11:43:00 -0500

    Add \ to clean up diff install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,10 @@ set -e
 dir="$(dirname "$(readlink -f "$0")")/"
 
 cd $HOME
-ln -s $dir.bash_logout $dir.bash_profile $dir.bashrc $dir.inputrc $dir.prettierrc $dir.profile $dir.vimrc .
+ln -s $dir.bash_logout $dir.bash_profile \
+	$dir.bashrc $dir.inputrc $dir.prettierrc \
+	$dir.profile $dir.vimrc .
 
 cd $XDG_CONFIG_HOME
-ln -s $dir.config/hypr/ $dir.config/kitty/ $dir.config/waybar/ .
+ln -s $dir.config/hypr/ $dir.config/kitty/ \
+	$dir.config/waybar/ .

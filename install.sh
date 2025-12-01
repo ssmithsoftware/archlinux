5d7bdd509f78e7029f4f265b896ab72734d5a023
scott <scottadamsmith1@gmail.com>
2025-12-01 13:16:25 -0500

    Add more dialogue to install.sh

--- install.sh
+++ install.sh
@@ -3,23 +3,28 @@
 set -e
 
 dir="$(dirname "$(readlink -f "$0")")/"
-msg='You must reboot to view your changes.'
+msg='Creating symlinks in '
+prompt='You must reboot to view your changes.'
 
 cd $HOME
+echo $msg$HOME
+
 ln -s $dir.bash_logout $dir.bash_profile \
 	$dir.bashrc $dir.gitconfig $dir.gittemplate/ \
 	$dir.inputrc $dir.prettierrc $dir.profile $dir.vimrc .
 
 cd $XDG_CONFIG_HOME
+echo $msg$XDG_CONFIG_HOME
+
 ln -s $dir.config/hypr/ $dir.config/kitty/ \
     $dir.config/uwsm/ $dir.config/waybar/ .
 
-read -p "$msg Would you like to reboot now? (y/n): " input
+read -p "$prompt Would you like to reboot now? (y/n): " input
 
 case $input in
     [Yy]*) echo 'Rebooting...';;
     [Nn]*) echo 'Exiting...'; exit;;
-    *) echo "Invalid input. $msg"; exit 1;;
+    *) echo "Invalid input. $prompt"; exit 1;;
 esac
 
 reboot

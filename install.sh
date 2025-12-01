f01a51bfe3f11e45af388548a50850a2cd6e80b7
scott <scottadamsmith1@gmail.com>
2025-12-01 15:47:55 -0500

    Add .editorconfig to symlinking

--- install.sh
+++ install.sh
@@ -9,22 +9,22 @@ prompt='You must reboot to view your changes.'
 cd $HOME
 echo $msg$HOME
 
-ln -s $dir.bash_logout $dir.bash_profile \
-	$dir.bashrc $dir.gitconfig $dir.gittemplate/ \
+ln -s $dir.bash_logout $dir.bash_profile $dir.bashrc \
+	$dir.editorconfig $dir.gitconfig $dir.gittemplate/ \
 	$dir.inputrc $dir.prettierrc $dir.profile $dir.vimrc .
 
 cd $XDG_CONFIG_HOME
 echo $msg$XDG_CONFIG_HOME
 
 ln -s $dir.config/hypr/ $dir.config/kitty/ \
-    $dir.config/uwsm/ $dir.config/waybar/ .
+	$dir.config/uwsm/ $dir.config/waybar/ .
 
 read -p "$prompt Would you like to reboot now? (y/n): " input
 
 case $input in
-    [Yy]*) echo 'Rebooting...';;
-    [Nn]*) echo 'Exiting...'; exit;;
-    *) echo "Invalid input. $prompt"; exit 1;;
+	[Yy]*) echo 'Rebooting...';;
+	[Nn]*) echo 'Exiting...'; exit;;
+	*) echo "Invalid input. $prompt"; exit 1;;
 esac
 
 reboot

4d81966a4c17291220376062c9075e5148f461ab
scott <scottadamsmith1@gmail.com>
2025-12-01 11:14:25 -0500

    Double quote install.sh to be safe from renaming

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd "$(dirname "$(readlink -f $0)")/"
+cd "$(dirname "$(readlink -f "$0")")/"
 
 # Add symlinks to user home directory
 ln -s .bash_logout .bash_profile .bashrc .inputrc .pretterrc .profile .vimrc $HOME

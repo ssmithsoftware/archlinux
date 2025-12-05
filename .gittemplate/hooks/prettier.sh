a4f1a650fc8993c4587841413d3b74ff6395314c
scott <scottadamsmith1@gmail.com>
2025-12-05 10:28:22 -0500

    Quote $files for zero length test command

--- .gittemplate/hooks/prettier.sh
+++ .gittemplate/hooks/prettier.sh
@@ -4,7 +4,7 @@ set -e
 
 # Get names of staged files added, copied, modified, or renamed. Exit if zero
 files=$(git diff --cached --diff-filter=ACMR --name-only | sed 's/ /\\ /g')
-[ -z $files ] && exit 0
+[ -z "$files" ] && exit 0
 
 # Format staged files
 echo $files | xargs prettier --ignore-unknown --write >/dev/null

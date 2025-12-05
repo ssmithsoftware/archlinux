9bffb5657634aecc2b44d84cbc9717f1b71bbbc3
scott <scottadamsmith1@gmail.com>
2025-12-05 08:59:06 -0500

    Update 256.sh with tabs and proper variables

--- 256.sh
+++ 256.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # Show 256 terminal colors
-for C in {0..255}; do
-    tput setaf $C
-    echo -n "$C "
+for color in {0..255}; do
+	tput setaf $color
+	echo -n "$color "
 done
 
 tput sgr0

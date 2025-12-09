defbb67369bc28047cf3a4329a21b145620a9da0
scott <scottadamsmith1@gmail.com>
2025-12-09 08:42:47 -0500

    Force new symlinks adding ln -f for idempotency

--- install.sh
+++ install.sh
@@ -9,14 +9,14 @@ prompt='You must reboot to view your changes.'
 cd $HOME
 echo $msg$HOME
 
-ln -s $dir.bash_logout $dir.bash_profile $dir.bashrc \
+ln -fs $dir.bash_logout $dir.bash_profile $dir.bashrc \
 	$dir.editorconfig $dir.gitconfig $dir.gittemplate/ \
 	$dir.inputrc $dir.prettierrc $dir.profile $dir.vimrc .
 
 cd $XDG_CONFIG_HOME
 echo $msg$XDG_CONFIG_HOME
 
-ln -s $dir.config/hypr/ $dir.config/kitty/ \
+ln -fs $dir.config/hypr/ $dir.config/kitty/ \
 	$dir.config/uwsm/ $dir.config/waybar/ .
 
 read -p "$prompt Would you like to reboot now? (y/n): " input

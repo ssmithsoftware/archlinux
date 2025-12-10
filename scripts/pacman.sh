b54a44e9a1dc82fd37457438ba5df5f1689509ab
scott <scottadamsmith1@gmail.com>
2025-12-10 14:29:43 -0500

    Simplify hook Exec using /etc/pacman.d/hooks and add cp to pacman.sh

--- scripts/pacman.sh
+++ scripts/pacman.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
+# Add dr9n mirrors and pacman hooks
 
+dir=/etc/pacman.d
 file=/etc/pacman.conf
 
 if ! grep -qs '^\[dr9n\]$' $file; then
+	echo "Adding dr9n mirrors to $file"
+
 	sudo tee -a $file >/dev/null <<-EOF
 
 		[dr9n]
 		Server = https://static.ssmithsoftware.com/archlinux/\$repo/os/\$arch
 	EOF
 fi
+
+sudo cp -rv $XDG_CONFIG_HOME/pacman/hooks/ $dir/hooks/

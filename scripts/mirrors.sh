7ae870d7b900fe1c4ef04ca7b813ce2e89185587
scott <scottadamsmith1@gmail.com>
2025-12-10 11:48:55 -0500

    Remove ssmithsoftware.com from mirrorlist

--- scripts/mirrors.sh
+++ scripts/mirrors.sh
@@ -8,18 +8,16 @@ prompt=$(cat <<-EOF
 	Are you sure you want to continue?
 EOF
 )
-url='https://static.ssmithsoftware.com/archlinux/$repo/os/$arch'
 
 read -p "$prompt (y/n): " input
 
 case $input in
-	[Yy]*) echo "Generating latest mirrors in $file. Please wait...";;
+	[Yy]*) echo "Generating latest mirrors in $file.\nPlease wait...";;
 	[Nn]*) echo 'Exiting'; exit;;
 	*) echo 'Invalid input.'; exit 1;;
 esac
 
-# Get top ten latest synchronized mirrors sorted by rate.
+# Get top ten latest synchronized mirrors sorted by download rate.
 sudo reflector -c US,CA,GB -l 10 -p https --save $file --sort rate
 
-echo "Appending $url to $file"
-echo "Server = $url" | sudo tee -a $file >/dev/null
+echo Done

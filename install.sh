08acc47dd04e925fe062e026ceb630780f1c78cc
scott <scottadamsmith1@gmail.com>
2025-12-11 15:43:10 -0500

    Lots of updates combine into single install script

--- install.sh
+++ install.sh
@@ -3,20 +3,33 @@
 
 set -e
 
-dir=$(dirname "$(readlink -f "$0")")/home/user
+dir=$(dirname "$(readlink -f "$0")")
 prompt='Reboot to view any desktop environment changes.'
 
-cd "$dir"/HOME
+cd "$dir"/home/user/HOME/
 ln -fsv "$PWD"/.* $HOME
 
-cd "$dir"/XDG_CONFIG_HOME
+cd ../XDG_CONFIG_HOME/
 ln -fsv "$PWD"/* $XDG_CONFIG_HOME
+
+# Include drop-in configuration directory to pacman.conf
+file=/etc/pacman.conf
+
+if ! grep -qs '^Include = /etc/pacman\.conf\.d/\*\.conf$' $file; then
+	sudo sed -i '/^\[options\]$/a Include = /etc/pacman.conf.d/*.conf\n' $file \
+		&& echo 'Drop-in directory appended to pacman.conf'
+fi
+
+# Copy drop-in directories and files
+cd "$dir"
+
+sudo cp -v etc/makepkg.conf.d/makepkg.conf /etc/makepkg.conf.d/
+sudo cp -rv etc/pacman.conf.d/ /etc/
+sudo cp -rv etc/pacman.d/hooks/ /etc/pacman.d/
 
 read -p "$prompt Would you like to reboot now? (y/n): " input
 case $input in
-	[Yy]*) echo 'Rebooting';;
-	[Nn]*) echo 'Exiting'; exit;;
+	[Yy]*) echo 'Rebooting'; reboot;;
+	[Nn]*) echo 'Exiting';;
 	*) echo "Invalid input. $prompt"; exit 1;;
 esac
-
-reboot

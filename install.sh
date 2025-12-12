03f7a89e67b9206491b8d0bd05322804fa63016f
scott <scottadamsmith1@gmail.com>
2025-12-12 09:36:59 -0500

    pacman hooks/ should not be needed. Remove reflector.conf, slows boot times. Rename home/user/ home/USER/. Move makepkg.conf to XDG_CONFIG_HOME. Modify install.sh

--- install.sh
+++ install.sh
@@ -6,29 +6,34 @@ dir=$(dirname "$(readlink -f "$0")")
 prompt='Reboot to view any desktop environment changes.'
 
 # Create symlinks in current user's home directory
-cd "$dir"/home/user/HOME/
-ln -fsv "$PWD"/.* $HOME
+cd "$dir"/home/USER/HOME/
+ln -fsv "$PWD"/.* $HOME/
 
 cd ../XDG_CONFIG_HOME/
-ln -fsv "$PWD"/* $XDG_CONFIG_HOME
+ln -fsv "$PWD"/* $XDG_CONFIG_HOME/
 
 # Include pacman drop-in configurations
 file=/etc/pacman.conf
 
 if ! grep -qs '^Include = /etc/pacman\.conf\.d/\*\.conf$' $file; then
-	sudo sed -i '/^\[options\]$/a Include = /etc/pacman.conf.d/*.conf\n' $file
-
-	echo 'Drop-in directory appended to pacman.conf'
+	printf "\n[options]\nInclude = /etc/pacman.conf.d/*.conf" \
+		| sudo tee -a $file >/dev/null 2>&1
 fi
 
 cd "$dir"/etc/
-sudo cp -v "$PWD"/makepkg.conf.d/makepkg.conf /etc/makepkg.conf.d/
-sudo cp -rv "$PWD"/pacman.conf.d/ /etc/
-sudo cp -rv "$PWD"/pacman.d/hooks/ /etc/pacman.d/
+sudo install -m755 "$PWD"/pacman.conf.d/ /etc/
 
-# Copy reflector configuration and enable systemd service
-sudo cp -v "$PWD"/xdg/reflector/reflector.conf /etc/xdg/reflector/
-sudo systemctl enable reflector.service
+# Get top 10 of 25 latest synchronized mirrors sorted by download rate
+read -p "Would you like to retrieve the latest pacman mirrors? (y/n): " input
+case $input in
+	[Yy]*)
+		echo 'Please wait...'
+		sudo reflector -c US,CA,GB -l 25 -n 10 -p https \
+			--save /etc/pacman.d/mirrorlist --sort rate --verbose
+
+		echo 'Done';;
+	*) echo 'Skipping mirrors';;
+esac
 
 read -p "$prompt Would you like to reboot now? (y/n): " input
 case $input in

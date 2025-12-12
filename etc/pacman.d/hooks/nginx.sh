709f954cf6d846fc2964b27de95f02ca608964f9
scott <scottadamsmith1@gmail.com>
2025-12-12 14:12:58 -0500

    Add types_hash fix as nginx Install hook

--- etc/pacman.d/hooks/nginx.sh
+++ etc/pacman.d/hooks/nginx.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-echo 'Need to insert types_hash fix'
+# https://wiki.archlinux.org/title/Nginx#Warning:_Could_not_build_optimal_types_hash
+sed -i '/^http {$/a\
+    types_hash_max_size 4096;\n    server_names_hash_bucket_size 128;\n' \
+		/etc/nginx/nginx.conf

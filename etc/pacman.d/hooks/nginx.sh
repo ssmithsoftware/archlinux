b72ade8b6be519439684b6e5f4489cd5f0601770
scott <scottadamsmith1@gmail.com>
2026-06-10 08:58:00 -0400

    install conf.d/ directory

--- etc/pacman.d/hooks/nginx.sh
+++ etc/pacman.d/hooks/nginx.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+path=/etc/nginx
+
+install -d $path/conf.d/
+
 # https://wiki.archlinux.org/title/Nginx#Warning:_Could_not_build_optimal_types_hash
 sed -i '/^http {$/a\
     types_hash_max_size 4096;\n    server_names_hash_bucket_size 128;\n' \
-		/etc/nginx/nginx.conf
+		$path/nginx.conf
 
 sudo systemctl --now enable nginx.service
